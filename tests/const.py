d0a6c4d470a8e478d9af6864835be604260ec25c
drc38 <derek_Caudwell@hotmail.com>
2022-06-11 13:06:22 +1200

    initial

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for DiUS_Powersensor tests."""
+from custom_components.dius.const import (
+    CONF_PASSWORD,
+)
+from custom_components.dius.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
