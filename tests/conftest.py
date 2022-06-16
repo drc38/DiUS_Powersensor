6ed91935e6077323125119208d870bcab8915129
drc38 <derek_Caudwell@hotmail.com>
2022-06-16 10:53:39 +0000

    allow sockets

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,12 @@ from custom_components.dius.api import (
 pytest_plugins = "pytest_homeassistant_custom_component"
 
 
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integrations defined in the test dir."""
+    yield
+
+
 # This fixture is used to prevent HomeAssistant from attempting to create and dismiss persistent
 # notifications. These calls would fail without this fixture since the persistent_notification
 # integration is never loaded during a test.
