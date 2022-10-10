94994439e0eb978eb808b303dd31b6ab4377de4a
drc38 <20024196+drc38@users.noreply.github.com>
2022-10-10 16:20:27 +1300

    Add extra option to tests

--- tests/const.py
+++ tests/const.py
@@ -8,4 +8,4 @@ from custom_components.dius.const import (
 
 MOCK_CONFIG_API = {CONF_HOST: "127.0.0.1", CONF_PORT: 49476}
 MOCK_CONFIG = {CONF_HOST: "192.168.1.1", CONF_PORT: 6789}
-MOCK_OPTIONS = {"sensor": True, "plug": False, "U_conv": 32.1}
+MOCK_OPTIONS = {"sensor": True, "plug": False, "U_conv": 32.1, "W_adj": -125}
