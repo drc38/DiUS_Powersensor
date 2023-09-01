787e5bd6d09198a9b54477abf24f99883f56e7f8
drc38 <20024196+drc38@users.noreply.github.com>
2023-09-01 15:32:40 +1200

    fix lingering tasks (#238)
    * fix lingering tasks
    
    * fix ref

--- custom_components/dius/__init__.py
+++ custom_components/dius/__init__.py
@@ -94,6 +94,8 @@ class DiusDataUpdateCoordinator(DataUpdateCoordinator):
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle removal of an entry."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
+    await coordinator.api.stop()
+
     unloaded = all(
         await asyncio.gather(
             *[
