81cb4b6bf63c6dfe288d71b797c08d51e59f6465
drc38 <20024196+drc38@users.noreply.github.com>
2024-11-07 13:57:36 +1300

    alter api test config

--- tests/test_api.py
+++ tests/test_api.py
@@ -74,7 +74,10 @@ async def test_api(hass, caplog, socket_enabled):
                 entry_id="testapi",
                 options=MOCK_OPTIONS,
             )
-            await async_setup_entry(hass, config_entry)
+            config_entry.add_to_hass(hass)
+            await hass.async_block_till_done()
+
+            await hass.config_entries.async_setup(config_entry.entry_id)
             await hass.async_block_till_done()
             # client = hass.data[DOMAIN][config_entry.entry_id].api
 
